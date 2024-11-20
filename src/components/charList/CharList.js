a05ee2d4f5263e9214588f3f8506f42accf9a57c
Ekaterina <Ek.zanoskina@gmail.com>
2024-11-20 22:14:01 +0300

    add focus and selected class to list items

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -9,27 +9,65 @@ class CharList extends Component {
     charList: [],
     loading: false,
     error: false,
+    newItemLoading: false,
+    offset: 210,
+    charEnded: false,
   };
   marvelService = new MarvelService();
 
   componentDidMount() {
+    this.onRequest();
+  }
+
+  onRequest = (offset) => {
+    this.onCharListLoading(); // на этапе первичной загрузки никак не повлияет, зато потом поможет внопку сделать disabled когда новые персонажи будут грузиться
     this.marvelService
-      .getAllCharacters()
+      .getAllCharacters(offset)
       .then(this.onCharListLoaded)
       .catch(this.onError);
-  }
-  onCharListLoaded = (charList) => {
+  };
+
+  onCharListLoading = () => {
     this.setState({
-      charList,
-      loading: false,
+      newItemLoading: true,
     });
+  };
+
+  onCharListLoaded = (newCharList) => {
+    let ended = false;
+    if (newCharList.length < 9) {
+      ended = true;
+    }
+    this.setState(({ offset, charList }) => ({
+      charList: [...charList, ...newCharList], // складываем два массива в один (новые и старые персонажи)
+      loading: false,
+      newItemLoading: false,
+      offset: offset + 9,
+      charEnded: ended,
+    }));
   };
   onError = () => {
     this.setState({ loading: false, error: true });
   };
 
+  
+  itemRefs = [];
+
+  setRef = (id) => {
+    this.itemRefs.push(id)
+  }
+
+  focusOnItem = (i) => {
+    this.itemRefs.forEach(item => {
+      item.classList.remove('char__item_selected')
+    })
+    this.itemRefs[i].classList.add('char__item_selected');
+    this.itemRefs[i].focus(); 
+  }
+
+
   renderItems(arr) {
-    const items = arr.map((item) => {
+    const items = arr.map((item, i) => {
       let imgStyle = { objectFit: "cover" };
       if (
         item.thumbnail ===
@@ -38,8 +76,17 @@ class CharList extends Component {
         imgStyle = { objectFit: "unset" };
       }
       return (
-        <li className="char__item" key={item.id}
-        onClick={() => this.props.onCharSelected(item.id)}
+        <li
+          className="char__item"
+          tabIndex={0}                  
+          key={item.id}
+          ref={this.setRef}
+          onClick={() => {this.props.onCharSelected(item.id); this.focusOnItem(i);}}
+          onKeyPress={(e) => {
+            if (e.key === ' ' || e.key === "Enter") {
+                this.props.onCharSelected(item.id);
+                this.focusOnItem(i);
+            }}}
         >
           <img src={item.thumbnail} alt={item.name} style={imgStyle} />
           <div className="char__name">{item.name}</div>
@@ -50,7 +97,8 @@ class CharList extends Component {
   }
 
   render() {
-    const { charList, loading, error } = this.state;
+    const { charList, loading, error, newItemLoading, offset, charEnded } =
+      this.state;
     const items = this.renderItems(charList);
     const errorMessage = error ? <ErrorMessage /> : null;
     const spinner = loading ? <Spinner /> : null;
@@ -60,7 +108,12 @@ class CharList extends Component {
         {errorMessage}
         {spinner}
         {content}
-        <button className="button button__main button__long">
+        <button
+          className="button button__main button__long"
+          disabled={newItemLoading}
+          style={{ display: charEnded ? "none" : "block" }}
+          onClick={() => this.onRequest(offset)}
+        >
           <div className="inner">load more</div>
         </button>
       </div>
--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -1,6 +1,8 @@
 class MarvelService {
   _apiBase = "https://gateway.marvel.com:443/v1/public/";
   _apiKey = "apikey=5854203285e89c0721ebd2c4326e65e3";
+  _baseOffset = 210;
+
   getReasource = async (url) => {
     let res = await fetch(url);
     if (!res.ok) {
@@ -9,9 +11,9 @@ class MarvelService {
     return await res.json();
   };
 
-  getAllCharacters = async () => {
+  getAllCharacters = async (offset = this._baseOffset) => {
     const res = await this.getReasource(
-      `${this._apiBase}characters?limit=9&offset=650&${this._apiKey}` //650
+      `${this._apiBase}characters?limit=9&offset=${offset}&${this._apiKey}` //650
     );
     return res.data.results.map(this._transformCharacter);
   };
